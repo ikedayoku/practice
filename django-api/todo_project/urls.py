15bdc0d70ed0a7de0e518816e4bc95102071f4b6
山田 <wataru90@gmail.com>
2024-03-01 18:19:40 +0900

    ユーザー登録api作成まで

--- django-api/todo_project/urls.py
+++ django-api/todo_project/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from user import views
+
+app_name = 'user'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+     path('create/', views.CreateUserView.as_view(), name='create'),
+     path('api/user/', include('user.urls')),
 ]
